992560d1ecccef7f97a21cd2233863eafa7b9538
Antério <anteriovieira@gmail.com>
2015-10-15 16:30:36 -0300

    Commands added

---
+++ src/VmProvisionCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace GestaoTI\Console;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class VmProvisionCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('vm:provision')
+            ->setDescription('Re-provisions the virtual machine')
+            ->addArgument('machine', InputArgument::REQUIRED, 'Virtual machine.');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $machine = $input->getArgument('machine');
+        $command = 'vagrant provision';
+        $path_machine = gestao_path().DIRECTORY_SEPARATOR.$machine;
+
+        $process = new Process($command, $path_machine, array_merge($_SERVER, $_ENV), null, null);
+        $process->run(function ($type, $line) use ($output, $machine,$path_machine) {
+            $output->write($line);
+        });
+    }
+}
