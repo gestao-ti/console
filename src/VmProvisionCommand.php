680d1849dcc897d05c228eec8575c6bf486b7ea5
Antério vieira da silva <anteriovieira@gmail.com>
2015-10-21 13:53:04 -0200

    Updated command

--- src/VmProvisionCommand.php
+++ src/VmProvisionCommand.php
@@ -36,7 +36,7 @@ class VmProvisionCommand extends Command
         $command = 'vagrant provision';
         $path_machine = gestao_path_vms().DIRECTORY_SEPARATOR.$machine;
 
-        $process = new Process($command, $path_machine);
+        $process = new Process($command, $path_machine, null, null, null);
         $process->run(function ($type, $line) use ($output, $machine,$path_machine) {
             $output->write($line);
         });
