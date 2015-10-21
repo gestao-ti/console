4104444d1c4195ffdfe5696d626981d7687ba92f
Antério vieira da silva <anteriovieira@gmail.com>
2015-10-21 13:52:02 -0200

    Updated command

--- src/VmHaltCommand.php
+++ src/VmHaltCommand.php
@@ -36,7 +36,7 @@ class VmHaltCommand extends Command
         $command = 'vagrant halt';
         $path_machine = gestao_path_vms().DIRECTORY_SEPARATOR.$machine;
 
-        $process = new Process($command, $path_machine);
+        $process = new Process($command, $path_machine, null, null, null);
         $process->run(function ($type, $line) use ($output, $machine,$path_machine) {
             $output->write($line);
         });
