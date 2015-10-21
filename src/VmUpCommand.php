c1a546c8db70e5fd736155a74f7ba5272f6b9dc5
Antério vieira da silva <anteriovieira@gmail.com>
2015-10-21 13:54:10 -0200

    Updated command

--- src/VmUpCommand.php
+++ src/VmUpCommand.php
@@ -40,7 +40,7 @@ class VmUpCommand extends Command
             (new VmInitCommand())->execute($input, $output);
         }
 
-        $process = new Process($command, $path_machine);
+        $process = new Process($command, $path_machine, null, null, null);
         $process->run(function ($type, $line) use ($output, $machine,$path_machine) {
             $output->write($line);
         });
