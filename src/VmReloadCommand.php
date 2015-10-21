81d84af8d69a8a248186c832b1a2a82482dcb35f
Antério vieira da silva <anteriovieira@gmail.com>
2015-10-21 14:00:59 -0200

    CS Fixes

--- src/VmReloadCommand.php
+++ src/VmReloadCommand.php
@@ -36,8 +36,8 @@ class VmReloadCommand extends Command
         $command = 'vagrant reload --provision';
         $path_machine = gestao_path_vms().DIRECTORY_SEPARATOR.$machine;
 
-        $process = new Process($command, $path_machine);
-        $process->run(function ($type, $line) use ($output, $machine,$path_machine, null, null, null) {
+        $process = new Process($command, $path_machine, null, null, null);
+        $process->run(function ($type, $line) use ($output, $machine,$path_machine) {
             $output->write($line);
         });
     }
