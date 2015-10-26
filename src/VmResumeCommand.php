88aa2b2542d352c39022a68bfb557bb4ca4ffce6
Antério vieira da silva <anteriovieira@gmail.com>
2015-10-26 10:18:07 -0200

    Added command resume

---
+++ src/VmResumeCommand.php
@@ -0,0 +1,40 @@
+<?php namespace GestaoTI\Console;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class VmResumeCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('vm:resume')
+            ->setDescription('Resume the suspended virtual machine')
+            ->addArgument('machine', InputArgument::REQUIRED, 'Virtual machine.');
+    }
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $machine = $input->getArgument('machine');
+        $command = 'vagrant resume';
+        $path_machine = gestao_path_vms().DIRECTORY_SEPARATOR.$machine;
+        $process = new Process($command, $path_machine, null, null, null);
+        $process->run(function ($type, $line) use ($output, $machine,$path_machine) {
+            $output->write($line);
+        });
+    }
+}
