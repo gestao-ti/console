10bcec9cdf16c90ff66114809804306fac2dd6c8
Anterio <anteriovieira@gmail.com>
2015-11-04 15:55:52 -0200

    Settings updated

--- src/scripts/serve-apache2.sh
+++ src/scripts/serve-apache2.sh
@@ -1,119 +1,70 @@
 #!/bin/bash
 
-### Set default parameters
-action=$1
-map=$2
-to=$3
-owner=$(who am i | awk '{print $1}')
-email='webmaster@localhost'
-pathSiteEnable='/etc/apache2/sites-enabled/'
-pathSiteAvailable='/etc/apache2/sites-available/'
-siteAvailableMap=$sitesAvailable$map.conf
+map=$1
+to=$2
+port=$3
 
-if [ "$action" == 'create' ]
-	then
-		### check if domain already exists
-		if [ -e $siteAvailableMap ]; then
-			echo -e $"Domain $map already exists."
-			exit;
-		fi
+pathSiteEnable="/etc/apache2/sites-enabled/"
+pathSiteAvailable="/etc/apache2/sites-available/"
+siteAvailableMap="$sitesAvailable$map.conf"
 
-		### check if directory exists or not
-		if ! [ -d $to ]; then
-			### create the directory
-			mkdir -p $to
-			### give permission to root dir
-			chmod 755 $to
-			### write test file in the new domain dir
-			if ! echo "<?php echo phpinfo(); ?>" > $to/phpinfo.php
-			then
-				echo $"ERROR: Not able to write in file $to/index.php. Please check permissions"
-				exit;
-			else
-				echo $"Added content to $to/index.php"
-			fi
-		fi
-
-		### create virtual host rules file
-		if ! echo "
-		<VirtualHost *:80>
-			ServerAdmin $email
-			ServerName $map
-			ServerAlias $map
-			DocumentRoot $to
-			<Directory />
-				AllowOverride All
-			</Directory>
-			<Directory $to>
-				Options Indexes FollowSymLinks MultiViews
-				AllowOverride all
-				Require all granted
-			</Directory>
-			ErrorLog /var/log/apache2/$map-error.log
-			LogLevel error
-			CustomLog /var/log/apache2/$map-access.log combined
-		</VirtualHost>" > $siteAvailableMap
-		then
-			echo -e $"There is an ERROR creating $map file"
-			exit;
-		else
-			echo -e $"\nNew Virtual Host Created\n"
-		fi
-
-
-
-		if [ "$owner" == "" ]; then
-			chown -R $(whoami):$(whoami) $to
-		else
-			chown -R $owner:$owner $to
-		fi
-
-		### enable website
-		a2ensite $map
-
-		### restart Apache
-		/etc/init.d/apache2 reload
-
-		### show the finished message
-		echo -e $"Complete! \nYou now have a new Virtual Host \nYour new host is: http://$map \nAnd its located at $to"
-		exit;
-	else
-		### check whether domain already exists
-		if ! [ -e $sitesAvailabledomain ]; then
-			echo -e $"This domain does not exist.\nPlease try another one"
-			exit;
-		else
-			### Delete domain in /etc/hosts
-			newhost=${domain//./\\.}
-			sed -i "/$newhost/d" /etc/hosts
-
-			### disable website
-			a2dissite $domain
-
-			### restart Apache
-			/etc/init.d/apache2 reload
-
-			### Delete virtual host rules files
-			rm $sitesAvailabledomain
-		fi
-
-		### check if directory exists or not
-		if [ -d $rootDir ]; then
-			echo -e $"Delete host root directory ? (y/n)"
-			read deldir
-
-			if [ "$deldir" == 'y' -o "$deldir" == 'Y' ]; then
-				### Delete the directory
-				rm -rf $rootDir
-				echo -e $"Directory deleted"
-			else
-				echo -e $"Host directory conserved"
-			fi
-		else
-			echo -e $"Host directory not found. Ignored"
-		fi
-
-		### show the finished message
-		echo -e $"Complete!\nYou just removed Virtual Host $domain"
-		exit 0;
+### check if domain already exists
+if [ -e $siteAvailableMap ]; then
+	### disable map
+	sudo a2dissite "$map.conf" > /dev/null && echo "Site $map disabled"
+	### restart Apache
+	sudo service apache2 restart > /dev/null
+	### Delete virtual host rules files
+	sudo rm -rf $siteAvailableMap
 fi
+
+### check if directory exists or not
+if ! [ -d $to ]; then
+	### create the directory
+	sudo mkdir -p $to
+	### give permission to root dir
+	sudo chmod 755 $to
+
+	# Create file index.php if not exist
+	if  ! [ -e "$to/index.php" ]; then
+		echo "<?php echo phpinfo(); " > $to/index.php
+		echo $"Added content to $to/index.php"
+	fi
+fi
+
+### create virtual host rules file
+if ! sudo echo "
+	<VirtualHost *:$port>
+		ServerAdmin webmaster@localhost
+		ServerName $map
+		ServerAlias $map
+		DocumentRoot $to
+		<Directory />
+			AllowOverride All
+		</Directory>
+		<Directory $to>
+			Options Indexes FollowSymLinks MultiViews
+			AllowOverride all
+			Require all granted
+		</Directory>
+		ErrorLog /var/log/apache2/$map-error.log
+		LogLevel error
+		CustomLog /var/log/apache2/$map-access.log combined
+	</VirtualHost>" > $siteAvailableMap
+then
+	echo -e $"There is an ERROR creating $map file"
+	exit;
+else
+	echo -e $"http://$map => $to"
+fi
+
+### enable site map
+sudo a2ensite "$map.conf" > /dev/null && echo "Enabling site $map"
+
+### restart Apache
+sudo service apache2 restart > /dev/null
+exit;
+
+
+
+
